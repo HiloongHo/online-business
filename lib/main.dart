33221e19700ca83fcd1fb9befe3b3b4eeb38113e
nathanhe <nathanhe@88vip.site>
2024-08-13 11:59:06 +0800

    前端完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
   // 组件依赖
 
   // 初始化本地存储
@@ -11,9 +13,10 @@ void main() {
   // 等待加载动画
 
   // 初始化 Firebase
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // 初始化验证
-
 
   runApp(const App());
 }
